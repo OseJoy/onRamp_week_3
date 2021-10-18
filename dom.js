8499cfec954874034fc634e40f9bf794e6636d51
OseJoy <joy2ose@gmail.com>
2021-10-18 17:54:04 +0100

    second commit

--- dom.js
+++ dom.js
@@ -1,8 +1,38 @@
-//variable for form id
-let form = document.getElementById('reservation');
-//form submit event
-form.addEventListener("click", checkClick())
-//check data function
-function checkClick(){
-    console.log("clicked");
+// var decrease = document.querySelector('decrease');
+// var increase = document.querySelector('increase');
+
+//  decrease.addEventListener('click', decreaseFunc())
+//  increase.addEventListener('click', increaseFunc())
+
+//  function increaseFunc(){
+//     let initVal
+//  }
+
+//  function decreaseFunc(){
+     
+//  }
+
+function incrementValue()
+{
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    if(value<50){
+        value++;
+        value = value+ " "+"People";
+            document.getElementById('number').value = value;
+    }
+}
+function decrementValue()
+{
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    if(value>1){
+        value--;
+        value = value+ " "+"People";
+            document.getElementById('number').value = value;
+    }else{
+        value = value+ " "+"Person";
+            document.getElementById('number').value = value;
+    }
+
 }
