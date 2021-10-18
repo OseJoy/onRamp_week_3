7a979006c89a9a594ed176f6811b614651ee1873
OseJoy <joy2ose@gmail.com>
2021-10-18 16:21:58 +0100

    First Commit

---
+++ dom.js
@@ -0,0 +1,8 @@
+//variable for form id
+let form = document.getElementById('reservation');
+//form submit event
+form.addEventListener("click", checkClick())
+//check data function
+function checkClick(){
+    console.log("clicked");
+}
